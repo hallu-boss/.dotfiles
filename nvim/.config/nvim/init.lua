1b2d16c5d57ce70049e5570a050578086aaa5f31
Paweł Hase <pawelhase@gmail.com>
2026-07-31 23:02:00 +0200

    changed plugins to mini

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,14 +2,7 @@ vim.pack.add {
 	'https://github.com/nvim-treesitter/nvim-treesitter',
 	'https://github.com/neovim/nvim-lspconfig',
   'https://github.com/mason-org/mason.nvim',
-	'https://github.com/stevearc/oil.nvim',
-	'https://github.com/vague-theme/vague.nvim',
-	'https://github.com/lewis6991/gitsigns.nvim',
-  'https://github.com/tpope/vim-fugitive',
-  'https://github.com/nvim-lua/plenary.nvim',
-  'https://github.com/nvim-telescope/telescope.nvim',
-  'https://github.com/nvim-telescope/telescope-ui-select.nvim',
-  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
+  'https://github.com/nvim-mini/mini.nvim',
 }
 
 require('mason').setup()
@@ -17,18 +10,31 @@ vim.lsp.enable {
   'lua_ls', 'vtsls',
 }
 
+require('mini.icons').setup()
+require('mini.ai').setup()
+require('mini.surround').setup()
+require('mini.pick').setup()
+require('mini.files').setup()
+require('mini.git').setup()
+require('mini.cmdline').setup()
+require('mini.completion').setup()
+require('mini.extra').setup()
+require('mini.diff').setup({
+  view = {
+    style = 'sign',
+    signs = { add = '+', change = '~', delete = '-' },
+  },
+})
+require('mini.hues').setup({ background = '#070912', foreground = '#c4c6cd' })
+
+
 require('neovide')
-require('find')
 
 vim.cmd.packadd('cfilter')
 vim.cmd.packadd('nvim.undotree')
 vim.cmd.packadd('nvim.difftool')
 
-require('oil').setup()
-
 require('vim._core.ui2').enable()
-
-vim.cmd.colorscheme('vague')
 
 vim.g.mapleader = ' '
 vim.opt.number = true
@@ -36,11 +42,9 @@ vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes'
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.completeopt = 'menu,menuone,fuzzy,noinsert'
 vim.opt.confirm = true
 vim.opt.termguicolors = true
-vim.opt.wildoptions:append { 'fuzzy' }
-vim.opt.statusline = '%<%f %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%) %P'
+vim.opt.statusline = "%<%f %h%m%r%{%get(b:, 'minigit_summary_string', '')%}%=%-14.(%l,%c%V%) %P"
 vim.opt.foldlevel = 999
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
@@ -48,40 +52,43 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
-require('telescope').setup {
-  extensions = {
-    ['ui-select'] = { require('telescope.themes').get_dropdown() },
-  },
-}
-
-pcall(require('telescope').load_extension, 'fzf')
-pcall(require('telescope').load_extension, 'ui-select')
-
-
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y')
 
-vim.keymap.set('n', '-', '<cmd>Oil<CR>')
+vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<CR>')
 
-local builtin = require 'telescope.builtin'
-vim.keymap.set('n', '<leader>f', builtin.find_files)
-vim.keymap.set('n', '<leader>b', builtin.buffers)
-vim.keymap.set('n', '<leader>.', builtin.oldfiles)
+vim.keymap.set('n', '<leader>f', '<cmd>Pick files<CR>')
+vim.keymap.set('n', '<leader>b', '<cmd>Pick buffers<CR>')
+vim.keymap.set('n', '<leader>\'', '<cmd>Pick resume<CR>')
+vim.keymap.set('n', '<leader>/', '<cmd>Pick grep_live<CR>')
+vim.keymap.set('n', '<leader>?', '<cmd>Pick help<CR>')
+vim.keymap.set('n', '<leader>h', '<cmd>Pick git_hunks<CR>')
+vim.keymap.set('n', '<leader>k', '<cmd>Pick keymaps<CR>')
+vim.keymap.set('n', '<leader>w', '<cmd>Pick grep pattern="<cword>"<CR>')
+vim.keymap.set('n', '<leader>m', '<cmd>Pick marks<CR>')
+
 vim.keymap.set('n', '<leader>t', ':terminal<CR>a')
-vim.keymap.set('n', '<leader>/', builtin.live_grep)
-vim.keymap.set('n', '<leader>?', builtin.help_tags)
-vim.keymap.set('n', '<leader>k', builtin.keymaps)
-vim.keymap.set('n', '<leader>j', builtin.jumplist)
-vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols)
-vim.keymap.set('n', '<leader>d', builtin.diagnostics)
-vim.keymap.set('n', '<leader>\'', builtin.resume)
-vim.keymap.set({ 'n', 'x' }, '<leader>w', builtin.grep_string)
 
-vim.keymap.set('n', 'grr', builtin.lsp_references)
-vim.keymap.set('n', 'gri', builtin.lsp_implementations)
-vim.keymap.set('n', 'grd', builtin.lsp_definitions)
-vim.keymap.set('n', 'gO', builtin.lsp_document_symbols)
-vim.keymap.set('n', 'gW', builtin.lsp_dynamic_workspace_symbols)
-vim.keymap.set('n', 'grt', builtin.lsp_type_definitions)
+-- local builtin = require 'telescope.builtin'
+-- vim.keymap.set('n', '<leader>f', builtin.find_files)
+-- vim.keymap.set('n', '<leader>b', builtin.buffers)
+-- vim.keymap.set('n', '<leader>.', builtin.oldfiles)
+-- vim.keymap.set('n', '<leader>t', ':terminal<CR>a')
+-- vim.keymap.set('n', '<leader>/', builtin.live_grep)
+-- vim.keymap.set('n', '<leader>?', builtin.help_tags)
+-- vim.keymap.set('n', '<leader>k', builtin.keymaps)
+-- vim.keymap.set('n', '<leader>j', builtin.jumplist)
+-- vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols)
+-- vim.keymap.set('n', '<leader>d', builtin.diagnostics)
+-- vim.keymap.set('n', '<leader>\'', builtin.resume)
+-- vim.keymap.set({ 'n', 'x' }, '<leader>w', builtin.grep_string)
+
+-- vim.keymap.set('n', 'grr', builtin.lsp_references)
+-- vim.keymap.set('n', 'gri', builtin.lsp_implementations)
+-- vim.keymap.set('n', 'grd', builtin.lsp_definitions)
+-- vim.keymap.set('n', 'gO', builtin.lsp_document_symbols)
+-- vim.keymap.set('n', 'gW', builtin.lsp_dynamic_workspace_symbols)
+-- vim.keymap.set('n', 'grt', builtin.lsp_type_definitions)
 
 vim.api.nvim_create_autocmd('FileType', {
     callback = function() pcall(vim.treesitter.start) end,
--- nvim/.config/nvim/lua/neovide.lua
+++ nvim/.config/nvim/lua/neovide.lua
@@ -1,4 +1,6 @@
 if vim.g.neovide then
+  vim.o.guifont = "Hurmit Nerd Font:h14"
+
 	vim.keymap.set('n', '<C-s-v>', '"+p')
 	vim.keymap.set('i', '<C-s-v>', '<C-r>+')
 	vim.keymap.set('t', '<C-s-v>', '<C-\\><C-o>"+p')
