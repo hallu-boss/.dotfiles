2d92365b258f5a44c72dfb912d6b77ebe34f08e1
Paweł Hase <pawelhase@gmail.com>
2026-07-31 21:29:06 +0200

    telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,6 +6,10 @@ vim.pack.add {
 	'https://github.com/vague-theme/vague.nvim',
 	'https://github.com/lewis6991/gitsigns.nvim',
   'https://github.com/tpope/vim-fugitive',
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/nvim-telescope/telescope.nvim',
+  'https://github.com/nvim-telescope/telescope-ui-select.nvim',
+  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
 }
 
 require('mason').setup()
@@ -44,16 +48,33 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+require('telescope').setup {
+  extensions = {
+    ['ui-select'] = { require('telescope.themes').get_dropdown() },
+  },
+}
+
+pcall(require('telescope').load_extension, 'fzf')
+pcall(require('telescope').load_extension, 'ui-select')
+
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 vim.keymap.set('n', '-', '<cmd>Oil<CR>')
-vim.keymap.set('n', '<leader>f', ':find ')
-vim.keymap.set('n', '<leader>b', ':buffer ')
-vim.keymap.set('n', '<leader>.', ':browse oldfiles<CR>')
+
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', '<leader>f', builtin.find_files)
+vim.keymap.set('n', '<leader>b', builtin.buffers)
+vim.keymap.set('n', '<leader>.', builtin.oldfiles)
 vim.keymap.set('n', '<leader>t', ':terminal<CR>a')
-vim.keymap.set('n', '<leader>/', ':copen | :silent grep ')
-vim.keymap.set('n', '<leader>w', ':copen | :silent grep <C-r><C-w><CR>')
+vim.keymap.set('n', '<leader>/', builtin.live_grep)
+vim.keymap.set('n', '<leader>?', builtin.help_tags)
+vim.keymap.set('n', '<leader>k', builtin.keymaps)
+vim.keymap.set('n', '<leader>j', builtin.jumplist)
+vim.keymap.set('n', '<leader>s', builtin.lsp_document_symbols)
+vim.keymap.set('n', '<leader>d', builtin.diagnostics)
+vim.keymap.set('n', '<leader>\'', builtin.resume)
+vim.keymap.set({ 'n', 'x' }, '<leader>w', builtin.grep_string)
 
 vim.api.nvim_create_autocmd('FileType', {
     callback = function() pcall(vim.treesitter.start) end,
