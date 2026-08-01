423af757365efdcdc009b781ac234660db3d70a6
Paweł Hase <pawelhase@gmail.com>
2026-08-01 23:00:14 +0200

    mini.files config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,7 +17,6 @@ require('vim._core.ui2').enable()
 
 require('mini.icons').setup()
 require('mini.pick').setup()
-require('mini.files').setup()
 require('mini.completion').setup()
 require('mini.diff').setup()
 require('mini.cmdline').setup()
@@ -93,4 +92,48 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd('FileType', {
     callback = function() pcall(vim.treesitter.start) end,
 })
+
+
+local MiniFiles = require('mini.files')
+MiniFiles.setup()
+
+-- Set focused directory as current working directory
+local set_cwd = function()
+  local path = (MiniFiles.get_fs_entry() or {}).path
+  if path == nil then return vim.notify('Cursor is not on valid entry') end
+  vim.fn.chdir(vim.fs.dirname(path))
+end
+
+-- Yank in register full path of entry under cursor
+local yank_path = function()
+  local path = (MiniFiles.get_fs_entry() or {}).path
+  if path == nil then return vim.notify('Cursor is not on valid entry') end
+  vim.fn.setreg(vim.v.register, path)
+end
+
+-- Open path with system default handler (useful for non-text files)
+local ui_open = function() vim.ui.open(MiniFiles.get_fs_entry().path) end
+
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'MiniFilesBufferCreate',
+  callback = function(args)
+    local b = args.data.buf_id
+    vim.keymap.set('n', 'g~', set_cwd,   { buffer = b, desc = 'Set cwd' })
+    vim.keymap.set('n', 'gX', ui_open,   { buffer = b, desc = 'OS open' })
+    vim.keymap.set('n', 'gy', yank_path, { buffer = b, desc = 'Yank path' })
+  end,
+})
+
+local set_mark = function(id, path, desc)
+    MiniFiles.set_bookmark(id, path, { desc = desc })
+  end
+  vim.api.nvim_create_autocmd('User', {
+    pattern = 'MiniFilesExplorerOpen',
+    callback = function()
+      set_mark('c', vim.fn.stdpath('config'), 'Config')
+      set_mark('w', vim.fn.getcwd, 'Working directory')
+      set_mark('~', '~', 'Home directory')
+      set_mark('d', '~/Downloads', 'Downloads directory')
+    end,
+  })
 
