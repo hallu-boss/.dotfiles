a955fcc1f5f2e5f8e8b851e85603387c02456280
Paweł Hase <pawelhase@gmail.com>
2026-08-01 22:52:08 +0200

    brand new config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,9 +2,8 @@ vim.pack.add {
 	'https://github.com/neovim/nvim-lspconfig',
   'https://github.com/mason-org/mason.nvim',
   'https://github.com/nvim-treesitter/nvim-treesitter',
-  'https://github.com/stevearc/oil.nvim',
   'https://github.com/tpope/vim-fugitive',
-  'https://github.com/nvim-tree/nvim-web-devicons',
+  'https://github.com/nvim-mini/mini.nvim',
 }
 
 vim.cmd.colorscheme('habamax')
@@ -16,7 +15,12 @@ vim.cmd.packadd('nvim.difftool')
 
 require('vim._core.ui2').enable()
 
-require('oil').setup()
+require('mini.icons').setup()
+require('mini.pick').setup()
+require('mini.files').setup()
+require('mini.completion').setup()
+require('mini.diff').setup()
+require('mini.cmdline').setup()
 
 -- NEOVIDE
 if vim.g.neovide then
@@ -62,32 +66,23 @@ vim.opt.termguicolors = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.completeopt = 'menu,menuone,fuzzy,noinsert'
+vim.opt.winborder = 'single'
 vim.opt.statusline = "%<%f %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%) %P"
-
-function Rg_find_files(cmdarg, _)
-  local fnames = vim.fn.systemlist("rg --files --hidden --color=never ")
-  if #cmdarg == 0 then
-    return fnames
-  else
-    return vim.fn.matchfuzzy(fnames, cmdarg)
-  end
-end
-
-vim.o.findfunc = "v:lua.Rg_find_files"
 
 -- KEYMAPS
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y')
 vim.keymap.set('n', '<leader>t', ':terminal<CR>a')
 
-vim.keymap.set('n', '-', '<cmd>Oil<CR>')
+vim.keymap.set('n', '<leader>e', '<cmd>lua MiniFiles.open()<CR>')
 
 vim.keymap.set('n', '<leader>.', '<cmd>browse oldfiles<CR>')
-vim.keymap.set('n', '<leader>f', ':find ')
-vim.keymap.set('n', '<leader>b', ':buffer ')
-vim.keymap.set('n', '<leader>/', ':grep ')
-vim.keymap.set('n', '<leader>w', ':grep <C-r><C-w><CR>')
+vim.keymap.set('n', '<leader>f', '<cmd>Pick files<CR>')
+vim.keymap.set('n', '<leader>b', '<cmd>Pick buffers<CR>')
+vim.keymap.set('n', '<leader>/', '<cmd>Pick grep_live<CR>')
+vim.keymap.set('n', '<leader>w', '<cmd>Pick grep pattern="<cword>"<CR>')
+vim.keymap.set('n', '<leader>\'', '<cmd>Pick resume<CR>')
+vim.keymap.set('n', '<leader>?', '<cmd>Pick help<CR>')
 vim.keymap.set('n', '<leader>g', '<cmd>tab G<CR>')
 
 -- AUTOCOMMANDS
@@ -99,14 +94,3 @@ vim.api.nvim_create_autocmd('FileType', {
     callback = function() pcall(vim.treesitter.start) end,
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(args)
-        vim.o.signcolumn = 'yes:1'
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        if client:supports_method('textDocument/completion') then
-            vim.o.complete = 'o,.,w,b,u'
-            vim.o.completeopt = 'menu,menuone,popup,noinsert'
-            vim.lsp.completion.enable(true, client.id, args.buf)
-        end
-    end
-})
