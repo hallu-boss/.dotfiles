d474e741827eb7584f0a8f23468552afeba6c442
Paweł Hase <pawelhase@gmail.com>
2026-08-02 15:37:35 +0200

    plugins

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,19 @@
 -- VIMRC
+vim.pack.add {
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/neovim/nvim-lspconfig',
+    'https://github.com/mason-org/mason.nvim',
+    'https://github.com/tpope/vim-fugitive',
+}
+
 vim.cmd.packadd('cfilter')
 vim.cmd.packadd('nvim.undotree')
 vim.cmd.packadd('nvim.difftool')
 
 require('vim._core.ui2').enable()
+
+require('mason').setup()
+vim.lsp.enable { 'lua_ls', 'vtsls' }
 
 vim.cmd.colorscheme('habamax')
 vim.cmd.colorscheme('default')
@@ -20,6 +30,7 @@ vim.o.tabstop = 2
 vim.o.undofile = true
 vim.o.confirm = true
 vim.opt.path:append { '**' }
+vim.o.statusline = "%<%f %h%w%m%r%{FugitiveStatusline()} %{% v:lua.require('vim._core.util').term_exitcode() %}%=%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &busy > 0 ? '◐ ' : '' %}%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) and vim.diagnostic.status() .. '' '') or '''' ') %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y')
@@ -32,6 +43,7 @@ vim.keymap.set('n', '<leader>f', ':find ')
 vim.keymap.set('n', '<leader>b', ':buffer ')
 vim.keymap.set('n', '<leader>/', ':copen | :silent grep! ')
 vim.keymap.set('n', '<leader>w', ':copen | :silent grep! <C-r><C-w><CR>')
+vim.keymap.set('n', '<leader>g', '<cmd>tab Git<CR>')
 
 vim.keymap.set('n', '<leader>t', ':terminal<CR>a')
 
