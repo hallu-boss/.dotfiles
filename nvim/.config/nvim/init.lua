4d22e2fcfec6c399e9662cd1ff46ef84794fcd95
Paweł Hase <pawelhase@gmail.com>
2026-08-02 23:34:18 +0200

    findfunc

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -41,8 +41,13 @@ vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.undofile = true
 vim.o.confirm = true
-vim.opt.path:append { '**' }
 vim.o.statusline = "%<%f %h%w%m%r%{FugitiveStatusline()} %{% v:lua.require('vim._core.util').term_exitcode() %}%=%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &busy > 0 ? '◐ ' : '' %}%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) and vim.diagnostic.status() .. '' '') or '''' ') %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
+
+function Find_files(text, _)
+  local files = vim.fn.glob('**/*', true, true)
+  return vim.fn.matchfuzzy(files, text)
+end
+vim.opt.findfunc = 'v:lua.Find_files'
 
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y')
