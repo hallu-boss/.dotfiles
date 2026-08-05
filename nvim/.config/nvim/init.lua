37444ef77587c818d3d0bf7896ba9fffca61ec19
Paweł Hase <pawelhase@gmail.com>
2026-08-05 23:16:13 +0200

    more configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,9 +14,35 @@ vim.o.wildmode = 'noselect'
 vim.opt.wildoptions:append { 'fuzzy' }
 
 vim.pack.add {
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/mason-org/mason.nvim',
+  'https://github.com/lewis6991/gitsigns.nvim',
   'https://github.com/vague-theme/vague.nvim',
   'https://github.com/ibhagwan/fzf-lua',
 }
+
+require('mason').setup()
+vim.lsp.enable {
+  'lua_ls'
+}
+
+local config = {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      workspace = {
+        library = {
+          vim.env.VIMRUNTIME,
+        }
+      },
+    },
+  },
+}
+
+vim.lsp.config('lua_ls', config)
 
 vim.cmd.colorscheme('vague')
 vim.api.nvim_set_hl(0, "FzfLuaBorder", { link = "BlinkCmpMenuBorder" })
@@ -37,6 +63,11 @@ vim.keymap.set('n', '<leader>w', '<cmd>FzfLua grep_cword<CR>')
 vim.keymap.set('x', '<leader>w', '<cmd>FzfLua grep_visual<CR>')
 vim.keymap.set('n', '<leader>h', '<cmd>FzfLua git_hunks<CR>')
 vim.keymap.set('n', '<leader>d', '<cmd>FzfLua diagnostics_document<CR>')
+
+vim.keymap.set({ 'n', 'x' }, 'gra', '<cmd>FzfLua lsp_code_actions<CR>')
+vim.keymap.set('n', 'gO', '<cmd>FzfLua lsp_document_symbols<CR>')
+vim.keymap.set('n', 'gri', '<cmd>FzfLua lsp_implementations<CR>')
+vim.keymap.set('n', 'grr', '<cmd>FzfLua lsp_references<CR>')
 
 vim.keymap.set('n', '<leader>v', '<cmd>edit $MYVIMRC<CR>')
 
