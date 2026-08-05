9d388e647787174e0a88e53bfbbc330d930e2709
Paweł Hase <pawelhase@gmail.com>
2026-08-05 17:41:00 +0200

    basic setup

---
+++ nvim/init.lua
@@ -0,0 +1,117 @@
+-- VIMRC
+vim.pack.add {
+    'https://github.com/nvim-treesitter/nvim-treesitter',
+    'https://github.com/neovim/nvim-lspconfig',
+    'https://github.com/mason-org/mason.nvim',
+    'https://github.com/lewis6991/gitsigns.nvim',
+}
+
+vim.cmd.packadd('cfilter')
+vim.cmd.packadd('nvim.undotree')
+vim.cmd.packadd('nvim.difftool')
+
+require('vim._core.ui2').enable()
+
+require('mason').setup()
+vim.lsp.enable { 'lua_ls', 'vtsls' }
+
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT', },
+      workspace = { library = { vim.env.VIMRUNTIME, } },
+    },
+  },
+})
+
+require('gitsigns').setup{
+  on_attach = function(bufnr)
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    map('n', '<leader>hq', function() gitsigns.setqflist('all') end)
+
+    -- Toggles
+    map('n', '\\b', gitsigns.toggle_current_line_blame)
+    map('n', '\\B', gitsigns.blame)
+    map('n', '\\w', gitsigns.toggle_word_diff)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', gitsigns.select_hunk)
+  end
+}
+
+vim.cmd.colorscheme('habamax')
+vim.cmd.colorscheme('default')
+
+vim.g.mapleader = ' '
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.signcolumn = 'yes'
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.undofile = true
+vim.o.confirm = true
+vim.o.statusline = "%<%f %h%w%m%r%{get(b:,'gitsigns_head','')} %{get(b:,'gitsigns_status','')} %{% v:lua.require('vim._core.util').term_exitcode() %}%=%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &busy > 0 ? '◐ ' : '' %}%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) and vim.diagnostic.status() .. '' '') or '''' ') %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
+
+function Rg_find_files(cmdarg, _)
+  local fnames = vim.fn.systemlist("rg --files --hidden --color=never ")
+  if #cmdarg == 0 then
+    return fnames
+  else
+    return vim.fn.matchfuzzy(fnames, cmdarg)
+  end
+end
+
+vim.opt.findfunc = 'v:lua.Rg_find_files'
+
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y')
+
+
+vim.keymap.set('n', '<leader>e', '<cmd>Ex<CR>')
+
+vim.keymap.set('n', '<leader>.', '<cmd>browse oldfiles<CR>')
+
+vim.keymap.set('n', '<leader>f', ':find ')
+vim.keymap.set('n', '<leader>b', ':buffer ')
+vim.keymap.set('n', '<leader>/', ':copen | :silent grep! ')
+vim.keymap.set('n', '<leader>g', ':terminal lazygit<CR>a')
+vim.keymap.set('n', '<leader>w', ':copen | :silent grep! <C-r><C-w><CR>')
+vim.keymap.set('x', '<leader>w', '"wy:copen | :silent grep! "<C-r>w"<CR>')
+
+vim.keymap.set('n', '<leader>t', ':terminal<CR>a')
+
+vim.api.nvim_create_autocmd("TermClose", {
+  pattern = 'term://*lazygit',
+  callback = function() vim.api.nvim_input('<CR>') end
+})
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function() vim.highlight.on_yank() end,
+})
