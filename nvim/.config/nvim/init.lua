8c561ca2f7b1aa3532fee539c1cd925b47abda0a
Paweł Hase <pawelhase@gmail.com>
2026-08-05 22:52:22 +0200

    restructure

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,5 @@
+require('vim._core.ui2').enable()
+
 vim.g.mapleader = ' '
 vim.o.number = true
 vim.o.relativenumber = true
@@ -5,44 +7,48 @@ vim.o.signcolumn = 'yes'
 vim.o.shiftwidth = 2
 vim.o.tabstop = 2
 vim.o.expandtab = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.confirm = true
+vim.o.wildmode = 'noselect'
 
 vim.pack.add {
   'https://github.com/vague-theme/vague.nvim',
-  'https://github.com/nvim-lua/plenary.nvim',
-  'https://github.com/nvim-telescope/telescope.nvim',
-  'https://github.com/nvim-telescope/telescope-ui-select.nvim',
-  'https://github.com/nvim-telescope/telescope-fzf-native.nvim',
-  'https://github.com/nvim-telescope/telescope-file-browser.nvim',
-  'https://github.com/nvim-tree/nvim-web-devicons',
-  'https://github.com/kdheepak/lazygit.nvim',
+  'https://github.com/ibhagwan/fzf-lua',
 }
 
 vim.cmd.colorscheme('vague')
+vim.api.nvim_set_hl(0, "FzfLuaBorder", { link = "BlinkCmpMenuBorder" })
 
-vim.g.lazygit_floating_window_scaling_factor = 0.96
+vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y')
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
-require('telescope').setup({
-  extensions = {
-    ['ui-select'] = { require('telescope.themes').get_dropdown() },
-    file_browser = { hijack_netrw = true },
-  },
-})
+vim.keymap.set('n', '<leader>e', '<cmd>Ex<CR>')
 
-pcall(require('telescope').load_extension, 'fzf')
-pcall(require('telescope').load_extension, 'ui-select')
-pcall(require('telescope').load_extension, 'file_browser')
+vim.keymap.set('n', '<leader>f', '<cmd>FzfLua files<CR>')
+vim.keymap.set('n', '<leader>b', '<cmd>FzfLua buffers<CR>')
+vim.keymap.set('n', '<leader>/', '<cmd>FzfLua live_grep<CR>')
+vim.keymap.set('n', '<leader>j', '<cmd>FzfLua jumps<CR>')
+vim.keymap.set('n', '<leader>\'', '<cmd>FzfLua resume<CR>')
+vim.keymap.set('n', '<leader>?', '<cmd>FzfLua helptags<CR>')
+vim.keymap.set('n', '<leader>k', '<cmd>FzfLua keymaps<CR>')
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>e', function() require("telescope").extensions.file_browser.file_browser() end)
-vim.keymap.set('n', '<leader>E', function() require("telescope").extensions.file_browser.file_browser({ path = vim.fs.dirname(vim.api.nvim_buf_get_name(0)) }) end)
-vim.keymap.set('n', '<leader>f', builtin.find_files)
-vim.keymap.set('n', '<leader>/', builtin.live_grep)
-vim.keymap.set('n', '<leader>\'', builtin.resume)
-vim.keymap.set('n', '<leader>?', builtin.help_tags)
-vim.keymap.set('n', '<leader>k', builtin.keymaps)
-vim.keymap.set({ 'n', 'x' }, '<leader>w', builtin.grep_string)
+vim.keymap.set('n', '<leader>v', '<cmd>edit $MYVIMRC<CR>')
 
-vim.keymap.set('n', '<leader>g', '<cmd>LazyGit<CR>')
-vim.keymap.set('n', '<leader>G', '<cmd>LazyGitFilterCurrentFile<CR>')
+vim.keymap.set('n', '<leader>g', '<cmd>terminal lazygit<CR>a')
 
 vim.keymap.set('n', '<leader>t', '<cmd>terminal<CR>a')
+
+vim.api.nvim_create_autocmd('CmdlineChanged', {
+  pattern = ':',
+  callback = function()
+    vim.fn.wildtrigger()
+  end,
+})
+
+vim.api.nvim_create_autocmd('TermClose', {
+  pattern = 'term://*lazygit',
+  callback = function()
+    vim.api.nvim_input('<CR>')
+  end,
+})
