9edf79b03f72fb224721975af1247f512333cf08
Paweł Hase <pawelhase@gmail.com>
2026-08-06 17:27:00 +0200

    options and keymaps

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -10,7 +10,9 @@ vim.o.expandtab = true
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.confirm = true
+vim.o.undofile = true
 vim.o.wildmode = 'noselect'
+vim.o.winborder = 'rounded'
 vim.opt.wildoptions:append { 'fuzzy' }
 
 vim.pack.add {
@@ -54,7 +56,6 @@ vim.keymap.set('n', '<leader>f', ':find ')
 vim.keymap.set('n', '<leader>b', ':buffer ')
 vim.keymap.set('n', '<leader>/', ':copen | silent grep! ')
 vim.keymap.set('n', '<leader>w', ':copen | silent grep! <C-r><C-w><CR>')
-vim.keymap.set('x', '<leader>w', 'y:copen | silent grep! escape("<C-r>"", " \\")<CR>')
 vim.keymap.set('n', '<leader>.', ':browse oldfiles<CR>')
 
 vim.keymap.set('n', '<leader>v', '<cmd>edit $MYVIMRC<CR>')
@@ -64,6 +65,8 @@ vim.keymap.set('n', '<leader>g', '<cmd>terminal lazygit<CR>a')
 vim.keymap.set('n', '<leader>t', '<cmd>terminal<CR>a')
 
 vim.keymap.set('n', '<leader>h', function() require("gitsigns").setqflist("all") end)
+vim.keymap.set('n', '[c', function() require("gitsigns").nav_hunk('prev') end)
+vim.keymap.set('n', ']c', function() require("gitsigns").nav_hunk('next') end)
 
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
