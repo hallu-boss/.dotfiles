7a0a5f03643b5cd8bf401eed130b0a3c1865b808
Paweł Hase <pawelhase@gmail.com>
2026-08-14 18:53:42 +0200

    added vim-fuguitive

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -20,13 +20,14 @@ vim.o.list = true
 vim.o.cursorline = true
 vim.opt.wildoptions:append { 'fuzzy' }
 vim.o.grepprg = 'rg --vimgrep'
-vim.o.statusline = "%<%f %h%w%m%r%{get(b:,'gitsigns_head','') !=# '' ? '(' .. get(b:,'gitsigns_head','') .. (get(b:,'gitsigns_status','') !=# '' ? ' ' .. get(b:,'gitsigns_status','') : '') .. ')' : ''} %{% v:lua.require('vim._core.util').term_exitcode() %}%=%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &busy > 0 ? '◐ ' : '' %}%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) and vim.diagnostic.status() .. '' '') or '''' ') %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
+vim.o.statusline = "%<%f %h%w%m%r%{FugitiveStatusline()} %{% v:lua.require('vim._core.util').term_exitcode() %}%=%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &busy > 0 ? '◐ ' : '' %}%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) and vim.diagnostic.status() .. '' '') or '''' ') %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerformat ) : '' %}"
 
 vim.pack.add {
   'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/neovim/nvim-lspconfig',
   'https://github.com/mason-org/mason.nvim',
   'https://github.com/lewis6991/gitsigns.nvim',
+  'https://github.com/tpope/vim-fugitive',
 }
 
 require('mason').setup()
